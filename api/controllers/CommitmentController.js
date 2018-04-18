cc183e2d86d3b0a4e75f413a27848413d866335b
Dave McKay <dave@polytiersoftware.com>
2018-04-18 16:47:26 -0400

    Added routing for REST api call to function

--- api/controllers/CommitmentController.js
+++ api/controllers/CommitmentController.js
@@ -15,6 +15,13 @@ module.exports = {
 		// Need to create a new user from this info
 		// user = Individual.new(source: source, sourceID: sourceID, firstName: firstName, lastName: lastName, email:email, isHelper: 0, isEntreprenuer: 0)
 
+	    User.create({
+	      firstName: req.param('first name'),
+	      lastName: req.param('last name'),
+	      messengerUserId: req.param('messenger user id'),
+	    })
+	    .exec(cb);
+	    return res.ok('Signup successful!');
 
 	},
 
