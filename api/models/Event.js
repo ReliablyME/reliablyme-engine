9b3bcfc96fe4234c8a5cb3a90eee61906a7c0cf3
Dave McKay <dave@polytiersoftware.com>
2018-05-07 13:04:14 -0400

    Added new model of events to get an event id

---
+++ api/models/Event.js
@@ -0,0 +1,38 @@
+/**
+ * Event.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    entreprenuer_id: {
+      type: 'string',
+      //required: true,
+      description: 'messenger user id of entrepreneur',
+    },
+
+    eventName: {
+      type: 'string',
+      //required: true,
+      description: 'Text name of the event to display',
+    },
+
+    eventLocation: {
+      type: 'string',
+      //required: true,
+      description: 'Text description of location to display',
+    },
+
+    eventDate: {
+      type: 'ref',
+      description: 'Date of event',
+      columnType: 'datetime',
+    },
+
+  }
+};
+
