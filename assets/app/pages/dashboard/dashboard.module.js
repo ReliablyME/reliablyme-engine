11656131150711cd93066d669b2caabb75e4a79f
Dave McKay <dave@polytiersoftware.com>
2018-05-08 21:13:30 -0400

    Removed stateprovider from Blur-admin

--- assets/app/pages/dashboard/dashboard.module.js
+++ assets/app/pages/dashboard/dashboard.module.js
@@ -1,19 +1,8 @@
 (function () {
   'use strict';
 
-  angular.module('reliablyMe.dashboard', []).config(routeConfig);
+  angular.module('reliablyMe.dashboard', []);
 
-  function routeConfig($stateProvider) {
-    $stateProvider
-        .state('dashboard', {
-          url: '/dashboard',
-          templateUrl: 'app/pages/dashboard/dashboard.html',
-          title: 'Dashboard',
-          sidebarMeta: {
-            icon: 'ion-android-home',
-            order: 0,
-          },
-        });
-  }
+ }
 
 })();
