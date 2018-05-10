da15ee416ce44c4d8507087bcf2e314f60c28946
Dave McKay <dave@polytiersoftware.com>
2018-05-09 23:53:29 -0400

    Fixed variable name as passed from ChatFuel

--- api/controllers/CommitmentController.js
+++ api/controllers/CommitmentController.js
@@ -226,8 +226,8 @@ module.exports = {
 
 	GetReliabilityRating: async function (req, res) {
 		console.log("Called GetReliabilityRating", req.allParams());
-		var commitmentsCompleteQuery = 'SELECT COUNT(*) AS complete FROM reliablyme.commitment WHERE helper_id=\'' + req.param("messengeruserid") +'\' AND commitmentStatus_id=5;';
-		var commitmentsQuery = 'SELECT COUNT(*) AS total FROM reliablyme.commitment WHERE helper_id=\'' + req.param("messengeruserid") + '\';';
+		var commitmentsCompleteQuery = 'SELECT COUNT(*) AS complete FROM reliablyme.commitment WHERE helper_id=\'' + req.param("messenger user id") +'\' AND commitmentStatus_id=5;';
+		var commitmentsQuery = 'SELECT COUNT(*) AS total FROM reliablyme.commitment WHERE helper_id=\'' + req.param("messenger user id") + '\';';
 		var params = [];
 
 		console.log("commitmentsCompleteQuery "+commitmentsCompleteQuery);
