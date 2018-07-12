adc894d58e832c328c16a3b190b053f8c8d5e805
gillbalraj <balrajgill18@gmail.com>
2018-07-12 19:26:37 -0400

    fixed bug- phoneNumber

--- api/models/User.js
+++ api/models/User.js
@@ -25,7 +25,7 @@ module.exports = {
       description: 'user phone number entered via chatbot',
       maxLength: 100,
       example: '1234567890'
-    }
+    },
 
     password: {
       type: 'string',
