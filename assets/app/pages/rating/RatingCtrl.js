fe4d4f7155b063fea2b6da9f61e28e51a5548ce5
gillbalraj <balrajgill18@gmail.com>
2018-07-18 16:43:01 -0400

    added user in ratingCtrl.js

--- assets/app/pages/rating/RatingCtrl.js
+++ assets/app/pages/rating/RatingCtrl.js
@@ -10,16 +10,20 @@
   /** @ngInject */
   function RatingCtrl($scope, $http, $location) {
 
-  	console.log("RatingCtrl!");
+    console.log("RatingCtrl!");
 
     $scope.completeTableData = [];
     $scope.incompleteTableData = [];
     $scope.Rating="?";
+    $scope.user = "?";
 
     $scope.loadTableData = function() { 
-	  var userId=$location.search().userid;
+    var userId=$location.search().userid;
       $http.post('/GetReliabilityRatingUser?userid='+userId).then(function(response) {
         $scope.Rating=response.data.set_attributes.Reliabilityrating;
+      })
+      $http.post('/GetReliabilityRatingUser?userid='+userId).then(function(response){
+        $scope.user=response.data.records;
       })
 
       $http.post('/GetCompleteUserList?userid='+userId).then(function(response) {
@@ -33,7 +37,7 @@
     $scope.loadTableData();
 
     $scope.refreshTable = function() {
-    	$scope.loadTableData();
+      $scope.loadTableData();
     };
   }
 
