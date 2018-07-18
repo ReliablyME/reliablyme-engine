9c528aca4ff27b19b19d5c6a1d44506bb65207c1
gillbalraj <balrajgill18@gmail.com>
2018-07-18 17:08:07 -0400

    added fullName

--- assets/app/pages/rating/RatingCtrl.js
+++ assets/app/pages/rating/RatingCtrl.js
@@ -23,7 +23,7 @@
         $scope.Rating=response.data.set_attributes.Reliabilityrating;
       })
       $http.post('/CommittmentList?userid='+userId).then(function(response){
-        $scope.user=response.data.records;
+        $scope.user=response.data.records.fullName;
       })
 
       $http.post('/GetCompleteUserList?userid='+userId).then(function(response) {
