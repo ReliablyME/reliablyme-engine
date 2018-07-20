c090eee331768c4646e17bcd448b4bfc6581151a
gillbalraj <balrajgill18@gmail.com>
2018-07-20 13:16:36 -0400

    test case 1

--- api/controllers/CommitmentController.js
+++ api/controllers/CommitmentController.js
@@ -442,7 +442,7 @@ module.exports = {
 	printUserName: async function (req, res) {
 		console.log("Called printUserName", req.allParams());
 		var printName = await User.find({where: {messengerUserId: req.param("userid")}});
-		res.json(printName);
+		res.json(printName[0].fullName);
 	   
 	},
 	CommittmentList: async function (req, res) {
