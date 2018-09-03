a231234ff5b0de907146457a489925f363f876f4
Dave McKay <dave@polytiersoftware.com>
2018-09-03 12:45:53 -0400

    Fixed quote style mismatch

--- api/controllers/CommitmentController.js
+++ api/controllers/CommitmentController.js
@@ -516,7 +516,7 @@ module.exports = {
 				JOIN reliablyme.user AS volunteer ON commit.helper_id=volunteer.messengerUserId 
 			    JOIN reliablyme.commitmentstatus AS comStat ON comStat.id=commit.commitmentStatus_id
 			    JOIN reliablyme.event AS events ON events.id=commit.event_id
-			    JOIN reliablyme.eventorganizer AS eventorg ON eventorg.event_id=commit.event_id AND eventorg.organizer_id=' + req.session.userId +'
+			    JOIN reliablyme.eventorganizer AS eventorg ON eventorg.event_id=commit.event_id AND eventorg.organizer_id=` + req.session.userId +`
 			    ORDER BY commit.commitmentDueDate, comStat.commitmentStatusName DESC, volunteer.prefFirstName; `;
 		
 		var params = [];
